ba6518d4059020ca03419b5511c722fd197baff1
BensonTochukwu <tecotochukwu@gmail.com>
2025-01-28 16:16:21 +0100

    Worked on the Javascript

--- main.js
+++ main.js
@@ -32,13 +32,16 @@ function closeMenu(){
 }
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbydSGGT9oogf8hpuSHrbQtf_FijVybrTZ3zAGOti-KIVrbFDAF-qY8b7xw0JaaDX9aKIQ/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz-js5DWrgdaO1811UofSnJ4QE4fhN7e5JjdkBA6kAJrdIoZgDlcRXxQoK93xvn-I8F-g/exec'
 const form = document.forms['submit-to-google-sheet']
 const msg = document.getElementById("msg")
 
 
 form.addEventListener('submit', e => {
   e.preventDefault()
+
+  msg.innerHTML = "Processing...";
+  
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response =>{
         msg.innerHTML = "Message sent successfully",
@@ -46,7 +49,9 @@ form.addEventListener('submit', e => {
             msg.innerHTML = ""
         }, 5000),
         form.reset()
-    }  
-    )
-    .catch(error => console.error('Error!', error.message))
-})
+    })
+    .catch((error) => {
+        console.error('Error!', error.message);
+        msg.innerHTML = "Error sending message. Please try again.";
+      });
+});
